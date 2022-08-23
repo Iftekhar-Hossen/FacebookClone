450976f5d316f1e445402ac1c249ce980431528d
Ifekhar Hossen <68419982+Iftekhar-Hossen@users.noreply.github.com>
2022-08-24 00:59:41 +0600

    files and folder initialization

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+/*
+    * import modules *
+ */
+
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
+
+/*
+    * * 
+ */
